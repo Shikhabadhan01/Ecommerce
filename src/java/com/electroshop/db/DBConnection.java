6819d7540b2ce83b5b8349dd7d505913ea5850e1
Shikha Badhan <162896164+Shikhabadhan01@users.noreply.github.com>
2025-04-30 16:40:09 +0530

    Update DBConnection.java

--- src/java/com/electroshop/db/DBConnection.java
+++ src/java/com/electroshop/db/DBConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/ElectroShop";
     private static final String USER = "root"; 
-    private static final String PASSWORD = "Shikha@8585"; 
+    private static final String PASSWORD = "your password"; 
 
     public static Connection getConnection() throws SQLException {
         try {
